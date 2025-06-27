92891c14e86d4f5ea03497fe5a3e3827220d8d92
Maya Robbestad <m.a.robbestad@gmail.com>
2025-06-27 13:28:44 +0200

    Move data folder, add JGraphT dependency, add synthetic data as edge list

---
+++ data/synthetic/directed/star-in.gml
@@ -0,0 +1,36 @@
+graph [
+    directed 1
+    n 5
+    m 3
+    node [
+        id 0
+    ]
+    node [
+        id 1
+    ]
+    node [
+        id 2
+    ]
+    node [
+        id 3
+    ]
+    node [
+        id 4
+    ]
+    edge [
+        source 1
+        target 0
+    ]
+    edge [
+        source 2
+        target 0
+    ]
+    edge [
+        source 3
+        target 0
+    ]
+    edge [
+        source 4
+        target 0
+    ]
+]
--- src/main/java/no/uib/mayarobbestad/dagcentrality/Main.java
+++ src/main/java/no/uib/mayarobbestad/dagcentrality/Main.java
@@ -1,17 +1,18 @@
 package no.uib.mayarobbestad.dagcentrality;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.nio.gml.GmlImporter;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Graph<String, DefaultEdge> g = new DefaultDirectedGraph<>(DefaultEdge.class);
+        g.addVertex("v1");
+        g.addVertex("v2");
+        g.addVertex("v3");
+        g.addEdge("v1", "v2");
+        g.addEdge("v2", "v3");
+        System.out.println(g);
     }
 }
