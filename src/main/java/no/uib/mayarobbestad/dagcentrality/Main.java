761ea8826f92291c89fb2a55962c2dcbd19b3f3b
Maya Robbestad <m.a.robbestad@gmail.com>
2025-06-30 16:24:37 +0200

    Add GraphBuilder class to read Graph from input file

--- src/main/java/no/uib/mayarobbestad/dagcentrality/Main.java
+++ src/main/java/no/uib/mayarobbestad/dagcentrality/Main.java
@@ -1,18 +1,51 @@
 package no.uib.mayarobbestad.dagcentrality;
 
+import no.uib.mayarobbestad.dagcentrality.graph.GraphBuilder;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.nio.gml.GmlImporter;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        Graph<String, DefaultEdge> g = new DefaultDirectedGraph<>(DefaultEdge.class);
-        g.addVertex("v1");
-        g.addVertex("v2");
-        g.addVertex("v3");
-        g.addEdge("v1", "v2");
-        g.addEdge("v2", "v3");
-        System.out.println(g);
+
+    static GraphBuilder builder = new GraphBuilder();
+
+    static int numberOfBlockchainGraphs = 2;
+    static int numberOfCompanyGraphs = 0;
+    static int numberOfSoftwareGraphs = 3;
+    static int numberOfDirectedSyntheticGraphs = 4;
+    static int numberOfUndirectedSyntheticGraphs = 3;
+
+    static ArrayList<String> directedSyntheticGraphs = new ArrayList<>(Arrays.asList(
+            "cycle", "path", "star-in", "star-out"));
+    static ArrayList<String> undirectedSyntheticGraphs = new ArrayList<>(Arrays.asList(
+            "clique-bridge", "clique-four", "clique-three"));
+
+    public static void main(String[] args) throws IOException {
+        printGraphs("data/synthetic/directed/", directedSyntheticGraphs);
+        printGraphs("data/synthetic/undirected/", undirectedSyntheticGraphs);
+
+    }
+
+    /**
+     * Prints graphs formated as an edgelist
+     * 
+     * @param rootPath
+     * @param graphs
+     */
+    public static void printGraphs(String rootPath, ArrayList<String> graphs) throws IOException {
+        System.out.println("-----Graphs from " + rootPath + "-----");
+        for (int i = 0; i < graphs.size(); i++) {
+            String graphData = graphs.get(i);
+            String path = rootPath + graphData + ".in";
+            System.out.println("Graph " + i + ": " + graphData);
+            System.out.println(builder.readGraphFromInputFile(path, true));
+        }
+        System.out.println("-------------------------------------------------------");
+
     }
 }
---
+++ src/main/java/no/uib/mayarobbestad/dagcentrality/graph/GraphBuilder.java
@@ -0,0 +1,48 @@
+package no.uib.mayarobbestad.dagcentrality.graph;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleDirectedGraph;
+import org.jgrapht.graph.SimpleGraph;
+
+import java.io.File;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * This class reads a graph from a file and stores the data in a JGraphT graph
+ * datastructure,
+ * took inspiration from INF102 semester assignment 2
+ */
+public class GraphBuilder {
+
+    /**
+     * Reads a simple graph from an edgelist file, where all vertices are
+     * represented as Integers.
+     *
+     * @param file
+     * @return
+     */
+    public Graph<Integer, DefaultEdge> readGraphFromInputFile(String file, boolean directed) throws IOException {
+        Scanner sc = new Scanner(new FileReader(new File(file)));
+        sc.useLocale(Locale.US);
+        int n = sc.nextInt(), m = sc.nextInt();
+        Graph<Integer, DefaultEdge> g;
+        if (directed) {
+            g = new SimpleDirectedGraph<>(DefaultEdge.class);
+        } else {
+            g = new SimpleGraph<>(DefaultEdge.class);
+        }
+        for (int u = 0; u < n; u++)
+            g.addVertex(u);
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt(), v = sc.nextInt();
+            g.addEdge(u, v);
+        }
+        return g;
+    }
+
+}
