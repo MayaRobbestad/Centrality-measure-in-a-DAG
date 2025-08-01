b695e65f34c40c156153b005e0ce589f573b3791
Maya Robbestad <m.a.robbestad@gmail.com>
2025-08-01 11:56:01 +0200

    Store the centrality scores in a results text file. Remove printGraphs class

--- src/main/java/no/uib/mayarobbestad/dagcentrality/Main.java
+++ src/main/java/no/uib/mayarobbestad/dagcentrality/Main.java
@@ -2,7 +2,9 @@ package no.uib.mayarobbestad.dagcentrality;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.net.IDN;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -10,6 +12,11 @@ import java.util.Scanner;
 
 import org.jgrapht.Graph;
 import org.jgrapht.alg.interfaces.VertexScoringAlgorithm;
+import org.jgrapht.alg.scoring.BetweennessCentrality;
+import org.jgrapht.alg.scoring.EigenvectorCentrality;
+import org.jgrapht.alg.scoring.HarmonicCentrality;
+import org.jgrapht.alg.scoring.KatzCentrality;
+import org.jgrapht.alg.scoring.PageRank;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.nio.gml.GmlImporter;
@@ -20,22 +27,196 @@ import no.uib.mayarobbestad.dagcentrality.graph.GraphBuilder;
 
 public class Main {
 
-    //
+    // builds the graph
     static GraphBuilder builder = new GraphBuilder();
 
     // The graphs to be run
     static ArrayList<Graph<Integer, DefaultEdge>> graphs = new ArrayList<>();
     static ArrayList<String> graphDirectory = new ArrayList<>();
 
+    // The centrality algorithms to be run
+    static final boolean DEGREE = true;
+    static final boolean INDEGREE = true;
+    static final boolean OUTDEGREE = true;
+    static final boolean CLOSENESS = true;
+    static final boolean INHARMONIC = true; // variation of closeness
+    static final boolean OUTHARMONIC = true;
+    static final boolean BETWEENNESS = true;
+    static final boolean EIGENVECTOR = true;
+    static final boolean KATZ = true;
+    static final boolean PAGERANK = true;
+    static final boolean MYDAGCENTRALITY = false;
+
     public static void main(String[] args) throws IOException {
         readAndStoreInputGraphs("data/dataFiles.txt", graphs, graphDirectory, true);
-        for (Graph<Integer, DefaultEdge> g : graphs) {
-            VertexScoringAlgorithm algo = new DegreeCentrality<>(g, true);
+        storeCentralityScores("results/results.txt", graphs);
+    }
 
-            System.out.println(algo.getScores());
-            algo = new DegreeCentrality<>(g, false);
-            System.out.println(algo.getScores());
+    /**
+     * Runs the algorithms that are set to true. These algorithms are stated as
+     * field variables.
+     * The results of the centrality measures are then stored in the file given
+     * 
+     * @param string
+     * @throws IOException
+     */
+    private static void storeCentralityScores(String file, ArrayList<Graph<Integer, DefaultEdge>> graphs)
+            throws IOException {
+        FileWriter writer = new FileWriter(file);
+        int numGraphs = graphs.size();
+        VertexScoringAlgorithm<Integer, Double> centralityAlgorithm;
+        if (DEGREE) {
+            writer.write("Degree centrality\n");
+            for (int i = 0; i < numGraphs; i++) {
+                writer.write(graphDirectory.get(i) + " = [");
+                centralityAlgorithm = new DegreeCentrality<>(graphs.get(i), true);
+                // makes sure that all vertices are written in the file, eventhopugh we might
+                // have deleted some vertieces previously
+                StringBuilder builder = new StringBuilder();
+                for (Integer v : graphs.get(i).vertexSet()) {
+                    builder.append(v + ":" + centralityAlgorithm.getVertexScore(v) + ",");
+                }
+                int n = builder.length();
+                builder.deleteCharAt(n - 1);
+                writer.write(builder.toString());
+                writer.write("]\n");
+            }
+            writer.write("\n");
         }
+        if (INDEGREE) {
+            writer.write("Indegree centrality\n");
+            for (int i = 0; i < numGraphs; i++) {
+                writer.write(graphDirectory.get(i) + " = [");
+                centralityAlgorithm = new DegreeCentrality<>(graphs.get(i), true, true);
+                StringBuilder builder = new StringBuilder();
+                for (Integer v : graphs.get(i).vertexSet()) {
+                    builder.append(v + ":" + centralityAlgorithm.getVertexScore(v) + ",");
+                }
+                int n = builder.length();
+                builder.deleteCharAt(n - 1);
+                writer.write(builder.toString());
+                writer.write("]\n");
+            }
+            writer.write("\n");
+        }
+        if (OUTDEGREE) {
+            writer.write("Outdegree centrality\n");
+            for (int i = 0; i < numGraphs; i++) {
+                writer.write(graphDirectory.get(i) + " = [");
+                centralityAlgorithm = new DegreeCentrality<>(graphs.get(i), false, true);
+                StringBuilder builder = new StringBuilder();
+                for (Integer v : graphs.get(i).vertexSet()) {
+                    builder.append(v + ":" + centralityAlgorithm.getVertexScore(v) + ",");
+                }
+                int n = builder.length();
+                builder.deleteCharAt(n - 1);
+                writer.write(builder.toString());
+                writer.write("]\n");
+            }
+            writer.write("\n");
+        }
+        if (INHARMONIC) {
+            writer.write("In-Harmonic centrality\n");
+            for (int i = 0; i < numGraphs; i++) {
+                writer.write(graphDirectory.get(i) + " = [");
+                centralityAlgorithm = new HarmonicCentrality<>(graphs.get(i), true, true);
+                StringBuilder builder = new StringBuilder();
+                for (Integer v : graphs.get(i).vertexSet()) {
+                    builder.append(v + ":" + centralityAlgorithm.getVertexScore(v) + ",");
+                }
+                int n = builder.length();
+                builder.deleteCharAt(n - 1);
+                writer.write(builder.toString());
+                writer.write("]\n");
+            }
+            writer.write("\n");
+        }
+        if (OUTHARMONIC) {
+            writer.write("Out-Harmonic centrality\n");
+            for (int i = 0; i < numGraphs; i++) {
+                writer.write(graphDirectory.get(i) + " = [");
+                centralityAlgorithm = new HarmonicCentrality<>(graphs.get(i), false, true);
+                StringBuilder builder = new StringBuilder();
+                for (Integer v : graphs.get(i).vertexSet()) {
+                    builder.append(v + ":" + centralityAlgorithm.getVertexScore(v) + ",");
+                }
+                int n = builder.length();
+                builder.deleteCharAt(n - 1);
+                writer.write(builder.toString());
+                writer.write("]\n");
+            }
+            writer.write("\n");
+        }
+
+        if (BETWEENNESS) {
+            writer.write("Betweenness centrality\n");
+            for (int i = 0; i < numGraphs; i++) {
+                writer.write(graphDirectory.get(i) + " = [");
+                centralityAlgorithm = new BetweennessCentrality<>(graphs.get(i), true);
+                StringBuilder builder = new StringBuilder();
+                for (Integer v : graphs.get(i).vertexSet()) {
+                    builder.append(v + ":" + centralityAlgorithm.getVertexScore(v) + ",");
+                }
+                int n = builder.length();
+                builder.deleteCharAt(n - 1);
+                writer.write(builder.toString());
+                writer.write("]\n");
+            }
+
+        }
+        writer.write("\n");
+        if (EIGENVECTOR) {
+            writer.write("Eigenvector centrality\n");
+            for (int i = 0; i < numGraphs; i++) {
+                writer.write(graphDirectory.get(i) + " = [");
+                centralityAlgorithm = new EigenvectorCentrality<>(graphs.get(i));
+                StringBuilder builder = new StringBuilder();
+                for (Integer v : graphs.get(i).vertexSet()) {
+                    builder.append(v + ":" + centralityAlgorithm.getVertexScore(v) + ",");
+                }
+                int n = builder.length();
+                builder.deleteCharAt(n - 1);
+                writer.write(builder.toString());
+                writer.write("]\n");
+            }
+
+        }
+        writer.write("\n");
+        if (KATZ) {
+            writer.write("Katz centrality\n");
+            for (int i = 0; i < numGraphs; i++) {
+                writer.write(graphDirectory.get(i) + " = [");
+                centralityAlgorithm = new KatzCentrality<>(graphs.get(i));
+                StringBuilder builder = new StringBuilder();
+                for (Integer v : graphs.get(i).vertexSet()) {
+                    builder.append(v + ":" + centralityAlgorithm.getVertexScore(v) + ",");
+                }
+                int n = builder.length();
+                builder.deleteCharAt(n - 1);
+                writer.write(builder.toString());
+                writer.write("]\n");
+            }
+
+        }
+        writer.write("\n");
+        if (PAGERANK) {
+            writer.write("PageRank centrality\n");
+            for (int i = 0; i < numGraphs; i++) {
+                writer.write(graphDirectory.get(i) + " = [");
+                centralityAlgorithm = new PageRank<>(graphs.get(i));
+                StringBuilder builder = new StringBuilder();
+                for (Integer v : graphs.get(i).vertexSet()) {
+                    builder.append(v + ":" + centralityAlgorithm.getVertexScore(v) + ",");
+                }
+                int n = builder.length();
+                builder.deleteCharAt(n - 1);
+                writer.write(builder.toString());
+                writer.write("]\n");
+            }
+        }
+        writer.write("\n");
+
+        writer.close();
 
     }
 
@@ -73,7 +254,7 @@ public class Main {
     }
 
     /**
-     * Given a list of graphs stored as ang edge list.
+     * Given a list of graphs stored as an edge list.
      * The method reads the graphs and stores the graphs in an array of graph
      * objects.
      * 
